e7aa80f929daf3033d34a46e8e5a14f8f22a074d
Bob Ippolito <bob@redivi.com>
2010-07-19 20:26:00 -0700

    worker processes

--- src/mochiweb_acceptor.erl
+++ src/mochiweb_acceptor.erl
@@ -9,29 +9,51 @@
 -include("internal.hrl").
 
 -export([start_link/3, init/3]).
+-export([worker/3]).
 
 start_link(Server, Listen, Loop) ->
     proc_lib:spawn_link(?MODULE, init, [Server, Listen, Loop]).
 
 init(Server, Listen, Loop) ->
-    T1 = now(),
     case catch mochiweb_socket:accept(Listen) of
         {ok, Socket} ->
-            gen_server:cast(Server, {accepted, self(), timer:now_diff(now(), T1)}),
-            call_loop(Loop, Socket);
+            spawn_call_loop(Loop, Socket);
         {error, closed} ->
-            exit(normal);
+            ok;
         {error, timeout} ->
-            exit(normal);
+            ok;
         {error, esslaccept} ->
-            exit(normal);
+            ok;
         Other ->
             error_logger:error_report(
               [{application, mochiweb},
                "Accept failed error",
                lists:flatten(io_lib:format("~p", [Other]))]),
             exit({error, accept_failed})
+    end,
+    init(Server, Listen, Loop).
+
+spawn_call_loop(Loop, Socket) ->
+    Pid = proc_lib:spawn(?MODULE, worker, [self(), Loop, Socket]),
+    case mochiweb_socket:controlling_process(Socket, Pid) of
+        ok ->
+            Pid ! self();
+        Other ->
+            error_logger:error_report(
+              [{application, mochiweb},
+               "Accept failure while setting controlling process",
+               lists:flatten(io_lib:format("~p", [Other]))]),
+            exit({error, accept_worker_failed})
     end.
+
+worker(Parent, Loop, Socket) ->
+    receive
+        Parent ->
+            ok
+    after 60000 ->
+            exit(normal)
+    end,
+    call_loop(Loop, Socket).
 
 call_loop({M, F}, Socket) ->
     M:F(Socket);
